f6fb3e437e3e80081e87862d045a171102c0ab27
José Carlos <carlosanchesm@hotmail.es>
2018-08-29 17:23:08 +0200

    proyecto base funcional

---
+++ StepTracker/Debug/main.d
@@ -0,0 +1,39 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/intrinsics.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/stdint.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/stdio.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/linkage.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/stdarg.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/stdlib.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/string.h
+main.obj: ../i2c.h
+main.obj: ../mma8451.h
+main.obj: ../PCD8544.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/stdio.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/stdlib.h
+main.obj: ../adc.h
+
+../main.c: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/intrinsics.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/stdlib.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/string.h: 
+../i2c.h: 
+../mma8451.h: 
+../PCD8544.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/stdlib.h: 
+../adc.h: 
